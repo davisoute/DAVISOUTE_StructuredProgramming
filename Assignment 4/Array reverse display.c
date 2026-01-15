b768cd5dbabd169a37d85403bc61c1b80d5b1a29
Davis Oute <outedavis@gmail.com>
2026-01-15 22:41:28 +0300

    Update Array reverse display.c

--- Assignment 4/Array reverse display.c
+++ Assignment 4/Array reverse display.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+    int numbers[3];
+    for (int i = 0; i < 3; i++)
+    {   printf("Enter integer elements: \n");
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("Values stored in the array are: %d %d %d\n",numbers[0], numbers[1], numbers[2]);
+
+    printf("Values stored in the array in reverse are: \n");
+    for (int j = 2; j >= 0; j--)
+    {
+       printf("%d ", numbers[j]);
+    }
+  
+    
+}
